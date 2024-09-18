cfadc8c832b91367944591fad539317cd72e115a
luanfsouza <luanferreira7777@gmail.com>
2024-09-18 11:22:25 -0300

    substituidos o projeto 01 e 02 por novos: buzz feed e ps-store

--- src/pages/api/project.ts
+++ src/pages/api/project.ts
@@ -10,27 +10,27 @@ export default async (req, res) => {
     {
       title: 'projeto-01',
       type: 'website',
-      imgUrl: '/project01.png',
+      imgUrl: '/ps-store.png',
       descricao:
-        'Woman HQ Ecommerce é onde voçê pode achar toda e qualquer historia em quadrino/manga feminino, normalmente é mais dificil achar esse tipo de conteudo entâo, porque nâo reuni-lo em um só lugar?',
-      site: 'https://woman-hq-ecommerce.vercel.app'
+        'Este projeto Angular tem como objetivo criar uma simulação da PlayStation Store, utilizando a abordagem de componentes atômicos para construir uma interface de usuário modular e reutilizável. Essa estratégia permite que os componentes sejam quebrados em partes menores e independentes, facilitando a manutenção e a escalabilidade do projeto.',
+      site: 'https://playstation-store-sand.vercel.app/'
     },
     {
       title: 'projeto-02',
+      type: 'website',
+      imgUrl: '/buzz-feed.png',
+      descricao:
+        'BuzzFeed Angular: Um quiz simples criado em Angular que determina se o usuário seria um super-herói, baseado em suas respostas. Utiliza um JSON como banco de dados para armazenar as perguntas e respostas.',
+      site: 'https://buzz-feed-nu.vercel.app/'
+    },
+    {
+      title: 'projeto-03',
       type: 'website',
       imgUrl: '/project02.png',
       descricao:
         'Costs é um site feito facilitar a vida das pessoas em questão financeira. Sinta-se livre para organizar seus projetos pessoais/profissionais.',
       site: 'https://costs-weld.vercel.app'
     },
-    {
-      title: 'projeto-03',
-      type: 'website',
-      imgUrl: '/project03.png',
-      descricao:
-        'Rede social feita com React JS, utulizando o banco de dados MongoDB juntamente com Node JS no backend',
-      site: 'https://react-social-media-black.vercel.app'
-    }
   ];
   try{
     res.status(200).send(projects)
